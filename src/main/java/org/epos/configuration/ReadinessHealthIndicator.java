a22a0ab15a4e3eeb102483d82ab71677f3ddb5d3
Valerio Vinciarelli <valerio.vinciarelli@epos-eric.eu>
2025-12-12 14:52:43 +0100

    test readiness and liveness update

--- src/main/java/org/epos/configuration/ReadinessHealthIndicator.java
+++ src/main/java/org/epos/configuration/ReadinessHealthIndicator.java
@@ -1,5 +1,13 @@
 package org.epos.configuration;
 
+import dao.EposDataModelDAO;
+import model.Person;
+import org.epos.Swagger2SpringBoot;
+import org.epos.router_framework.RpcRouter;
+import org.epos.router_framework.exception.RoutingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -7,19 +15,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReadinessHealthIndicator implements HealthIndicator {
 
-	@Override
-	public Health health() {
-		int errorCode = check();
-		if (errorCode != 0) {
-			return Health.down().withDetail("No Database Connection", errorCode).build();
-		}
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadinessHealthIndicator.class);
 
-		return Health.up().build();
-	}
+    @Autowired
+    private static RpcRouter router;
 
-	private int check() {
+    static {
+        try {
+            router.init(System.getenv("BROKER_HOST"), System.getenv("BROKER_VHOST"), System.getenv("BROKER_USERNAME"),
+                    System.getenv("BROKER_PASSWORD"));
+        } catch (RoutingException e) {
+            LOGGER.error("A problem was encountered whilst initialising the routing framework.", e);
+        }
+    }
 
-		return 0;
-
-	}
+    @Override
+    public Health health() {
+        try {
+            if(!router.doHealthCheck()){
+                return Health.down().withDetail("No Router Connection", 1).build();
+            }
+        }catch(Exception e){
+            return Health.down().withDetail("No Router Connection", 1).build();
+        }
+        try {
+            EposDataModelDAO.getInstance().getAllFromDB(Person.class);
+        }catch(Exception e){
+            return Health.down().withDetail("No Database Connection", 1).build();
+        }
+        return Health.up().build();
+    }
 }
